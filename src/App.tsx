68548f83494dd0e1a645749c3f305bb5945fd59b
Alexander1995-it <sany_nax@mail.ru>
2022-07-05 20:00:07 +0300

    up to 28 lessons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,57 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header'
+import Navbar from "./components/Navbar/Navbar";
+import Profile from './components/Profile/Profile'
+import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+export type MyProfileType = {
+    id: number,
+    name: string,
+    status: string,
+    ava: any
+}
+export type MyPostType = {
+    id: number,
+    post: string,
+    likes: number
+}
+
+export type ProfilePageType = {
+    myProfile: MyProfileType
+    myPost: Array<MyPostType>
+}
+
+export type InitialStateType = {
+    profilePage: ProfilePageType
+}
+
+export type RootStatePropsType = {
+    initialState: InitialStateType
+}
+
+
+
+function App(props: RootStatePropsType) {
+
+    return (
+        <BrowserRouter>
+            <div className="App-wrapper">
+                <Header/>
+                <div className='app-main__container'>
+                    <div className='app-main__wrapper'>
+                        <Navbar/>
+                        <div className='app-main__content'>
+                            <Route path='/profile' render={() => <Profile profile={props.initialState.profilePage}/>}/>
+                            <Route path='/dialogs' component={Dialogs}/>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App, {InitialStateType} from './App';
+import ava from './images/IMG_social-network.jpg'
+
+let initialState: InitialStateType = {
+    profilePage: {
+        myProfile: {
+            id: 1,
+            name: 'Alexander',
+            status: 'I am student Incubator',
+            ava: ava
+        },
+        myPost: [
+            {id:1, post: 'I am from Minsk', likes: 10},
+            {id:2, post: 'I am student of Incubator',  likes: 20},
+            {id:3, post: 'I am looking for a job',  likes: 8}
+        ]
+    }
+}
 
 ReactDOM.render(
-    <App />,
+    <App initialState={initialState}/>,
   document.getElementById('root')
 );
